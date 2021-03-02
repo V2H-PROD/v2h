257babd1d2833f37eae41a4a40b85c12f8404f42
V2H-PROD <79463968+V2H-PROD@users.noreply.github.com>
2021-03-02 11:31:56 +0100

    Update app.js

--- extra/app.js
+++ extra/app.js
@@ -2,3 +2,19 @@ $('.header_navbar-toggle').click(function(e) {
     e.preventDefault();
     $('.header_navbar').toggleClass('is-open');
 })
+
+/* chargement en bas */
+
+const load = document.querySelector('.load');
+
+window.addEventListener('load', () => {
+    load.classList.add('fondu-out');
+})
+
+/* animation des bulles de media en bas */
+
+const medias = document.querySelector('.bulle');
+
+window.addEventListener('load', () => {
+    medias.classList.add('fondu-medias');
+})
