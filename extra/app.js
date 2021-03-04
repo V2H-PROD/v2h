e43bfdddc75d979ed4d04e13a169cc1534fd9c88
V2H-PROD <79463968+V2H-PROD@users.noreply.github.com>
2021-03-04 08:25:55 +0100

    Update app.js

--- extra/app.js
+++ extra/app.js
@@ -1,17 +1,20 @@
+/* navbar burger */
+
 $('.header_navbar-toggle').click(function(e) {
     e.preventDefault();
     $('.header_navbar').toggleClass('is-open');
 })
 
-/* chargement en bas */
+/* chargement */
 
-const load = document.querySelector('.load');
+const load = document.querySelector('.loader-back'); 
 
 window.addEventListener('load', () => {
     load.classList.add('fondu-out');
-})
+});
 
-/* animation des bulles de media en bas */
+
+/* animation des bulles de media */
 
 const medias = document.querySelector('.bulle');
 
